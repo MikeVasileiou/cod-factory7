4130a25586d071c6bdfb0f056f78f2e4a552bc74
Mike.Vasileiou <michvasi@hotmail.com>
2025-01-26 21:49:17 +0200

    initial commit

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * Τυπώνει "Hello Coding Factory"
+ * στην κονσόλα
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Coding Factory!");
+    }
+
+}
